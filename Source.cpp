fb0b27a09305c49de47e80f3d4e6f8d67392d287
YanaLiso <114093396+YanaLiso@users.noreply.github.com>
2022-09-26 22:34:14 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//If7.Даны два числа.Вывести порядковый 
+	//номер меньшего из них.
+	
+	/*int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		cout << 1 << endl;
+	}
+	else (b > a);
+		cout << 2 << endl;*/
+
+	//If8◦.Даны два числа.Вывести вначале
+	//большее, а затем меньшее из них.
+
+	int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		cout << a  << ' ' << b;
+	}
+	else  {
+		cout << b  << ' ' << a;
+	}
+	
+
+}
